5307e68c9670c387222ae25686f385b573c43ac3
Vincent Runge <vrunge@tora.local>
2025-02-26 15:20:58 +0100

    update simus

--- simulations/normal_corrupted_tests.R
+++ simulations/normal_corrupted_tests.R
@@ -1,21 +1,34 @@
 
+###
+### normal distribution tests. Corruption. What to do? ECC test?
+###
 
-n <- 1000
+
+n <- 100000
 data1 <- rnorm(n, mean = 0, sd = 1)  # Generate 100 Gaussian data points
 hist(data1, breaks = 100)
 # Perform KS test against a standard normal distribution
 ks.test(data1, "pnorm", mean = 0, sd = 1)
 
 
-data2 <- data_gaussian_corrupted_1D(N = n, m = 0, s2 = 1, l = 1, r = 1.2)
+data2 <- data_gaussian_corrupted_1D(N = n, m = 0, s2 = 1, l = 1, r = 1.1)
 hist(data2, breaks = 100)
 ks.test(data2, "pnorm", mean = 0, sd = 1)
 
-data3 <- floor(data1*10)/10 + runif(length(data3), -1e-9, 1e-9)
+data3 <- floor(data1*10)/10 + runif(length(data1), -1e-6, 1e-6)
 hist(data3, breaks = 100)
 ks.test(data3, "pnorm", mean = 0, sd = 1)
 
 library(nortest)
+data3 <- floor(data1*100)/100
 ad.test(data3)  # Anderson-Darling test
 shapiro.test(data3)  # Shapiro-Wilk test (for small samples)
+
+data4 <- floor(data1*10)/10
+barplot(table(data4))
+ad.test(data4)  # Anderson-Darling test
+shapiro.test(data4)  # Shapiro-Wilk test (for small samples)
+
+
+
 
